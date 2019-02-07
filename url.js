706edae26417dea99be18f419a88a72ba219c4f4
Narendra <narendra21399@gmail.com>
2019-02-07 10:53:22 +0530

    first commit

---
+++ url.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const app=express();
+const bodyparser= require('body-parser');
+const fs = require('fs');
+const data={};
+var student;
+app.use(bodyparser.json());
+
+app.listen(4000,function(){
+  console.log('connected');
+});
+
+fs.readFile('data.json', (err, data) => {  
+    if (err) throw err;
+    student = JSON.parse(data);
+		let user4={
+					"name" : "Narendra",
+					"password" : "password3",
+					"profession" : "software dev",
+					"id": 4
+		}
+	
+	student.user4=user4;
+	console.log(student);
+});
+
+
+ 
+ 
+
+
+
+
+
+app.get('/login',function(req,res){
+	 console.log("hii");
+	 res.send(student);
+})
+
+
+app.post('/post',function(req,res){
+var name=req.body.name;
+var pass=req.body.pass;
+
+data.name=name;
+data.pass=pass;
+console.log(data);
+res.send(data);
+})
+
