49a874d36e3ab882a156f7edcc75ce61a0adc5bd
Narendra <narendra21399@gmail.com>
2019-02-11 15:37:47 +0530

    second commit

--- url.js
+++ url.js
@@ -1,51 +1,23 @@
-const express=require('express');
-const app=express();
-const bodyparser= require('body-parser');
+const http = require('http');
 const fs = require('fs');
-const data={};
-var student;
-app.use(bodyparser.json());
 
-app.listen(4000,function(){
-  console.log('connected');
+fs.readFile('index.html', (err, data) => {  
+if(err) throw err
+
+
+const server=http.createServer((req,res)=>{
+	res.statusCode=200;
+ res.setHeader('Content-type','text/html');
+ res.write(fs);
+ res.end();
+});
+server.listen(3030,()=>{
+console.log('server running at :3030');
 });
 
-fs.readFile('data.json', (err, data) => {  
-    if (err) throw err;
-    student = JSON.parse(data);
-		let user4={
-					"name" : "Narendra",
-					"password" : "password3",
-					"profession" : "software dev",
-					"id": 4
-		}
-	
-	student.user4=user4;
-	var p='screen';
-	 
 });
 
-
- 
  
 
 
-
-
-
-app.get('/login',function(req,res){
-	 console.log("hii");
-	 res.send(student);
-})
-
-
-app.post('/post',function(req,res){
-var name=req.body.name;
-var pass=req.body.pass;
-
-data.name=name;
-data.pass=pass;
-console.log(data);
-res.send(data);
-})
 
